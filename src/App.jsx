06c3b728dbc62f4f9cb4888876953937ed32f1cf
NicolasPFerraz <nicolaspaduaferraz54321@gmail.com>
2025-05-27 19:37:10 -0300

    chore: cria estrutura básica de rotas no 'App.jsx' e os principais componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, useParams, Routes, Navigate } from 'react-router-dom'
 import './App.css'
 
-function App() {
+import Home from './pages/Home'
 
+function HomeWrapper() {
+  const { section } = useParams() // 'hero', 'about', 'services' etc
+  return <Home section={section} />
+}
+
+function App() {
   return (
-    <>
-      <h1>App.jsx</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home activeSection={undefined} />} />
+        <Route path="/:section" element={<HomeWrapper />} />
+        <Route path="*" element={<Navigate to="/home" />} />
+      </Routes>
+    </Router>
   )
 }
 
