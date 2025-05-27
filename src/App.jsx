cdb158898260cb13605c5e24eeef62a9bf626bf4
NicolasPFerraz <nicolaspaduaferraz54321@gmail.com>
2025-05-27 19:17:19 -0300

    chore: inicia estrutura base do projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>App.jsx</h1>
+    </>
+  )
+}
+
+export default App
