34c732e8eadb046d178de9957557b14f1698421d
NICOLAS PÁDUA FERRAZ <nicolas.ferraz4@Industrial.edu>
2025-06-10 14:04:28 -0300

    feat: cria o layout principal da seção de testemunhos

--- src/components/HomeSections/Testimonials.jsx
+++ src/components/HomeSections/Testimonials.jsx
@@ -1,3 +1,22 @@
+import styles from './Testimonials.module.css';
+
 export default function Testimonials() {
-  return <h2>Testimonials</h2>
+  return (
+    <div className={styles.testimonialsContainer}>
+      <div className={styles.testimonialsTitle}>
+        <h2>O QUE DIZEM SOBRE A BarberShop?</h2>
+      </div>
+      <div className={styles.testimonialsCard}>
+        <div className={styles.testimonialCard}>
+
+        </div>
+        <div className={styles.testimonialCard}>
+
+        </div>
+        <div className={styles.testimonialCard}>
+
+        </div>
+      </div>
+    </div>
+  )
 }
